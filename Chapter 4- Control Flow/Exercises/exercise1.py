f645f3841b8a57b9c746a99601c3d69dd5efcd68
Saifullah007-ui <114293798+Saifullah007-ui@users.noreply.github.com>
2022-11-16 19:00:13 +0400

    chp 4 done

---
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -0,0 +1,9 @@
+Alien_colour = "Green"
+
+
+if Alien_colour == "Green" :
+ print("Player just earned 5 points!")
+ Alien_colour = "Yellow"
+
+if Alien_colour == "Green":
+  print ("Player just earned 5 points!")
---
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -0,0 +1,12 @@
+favorite_fruit = ["apples","mango","banana","orange","papaya"]
+
+if "apples" in favorite_fruit:
+ print("you really like apples")
+if "mango" in favorite_fruit:
+ print("you really like mango")
+if "banana" in favorite_fruit:
+ print("you really like banana")
+if "orange" in favorite_fruit:
+ print("you really like orange")
+if "papaya" in favorite_fruit:
+ print("you really like papaya")
